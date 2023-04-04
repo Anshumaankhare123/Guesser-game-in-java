603611e855cae2e400b579260b0119274dcf8a6f
Anshumaan khare <94692766+Anshumaankhare123@users.noreply.github.com>
2023-04-04 10:33:00 +0530

    Add files via upload

--- game.java
+++ game.java
@@ -6,12 +6,12 @@ class guesser {
 	int guessthenumber() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Guesser guess the number ");
+		System.out.println();
 		guessnumber = sc.nextInt();
 		return guessnumber;
 
 	}
 }
-
 
 class player {
 	int pgussernum;
@@ -19,25 +19,24 @@ class player {
 	int playergussthenumber() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("player guess the number");
+		System.out.println();
 		pgussernum = sc.nextInt();
 		return pgussernum;
 	}
 }
-
 
 class umpire {
 	int numFromGuesser;
 	int numFromPlayer1;
 	int numFromPlayer2;
 	int numFromPlayer3;
-
+	int j;
 
 	public void collectNumFromGuesser() {
 		guesser g = new guesser();
 		numFromGuesser = g.guessthenumber();
 
 	}
-
 
 	public void collectNumFromPlayer() {
 		player p1 = new player();
@@ -48,7 +47,6 @@ class umpire {
 		numFromPlayer2 = p2.playergussthenumber();
 		numFromPlayer3 = p3.playergussthenumber();
 	}
-
 
 	void compare() {
 		if (numFromGuesser == numFromPlayer1) {
@@ -74,17 +72,51 @@ class umpire {
 			System.out.println("you guess the wrong number");
 		}
 	}
-}
+	public void countandmacth(){
+		for(j=0y;j<3;j++){
+			umpire u = new umpire();
+		    u.collectNumFromGuesser();
+		    u.collectNumFromPlayer();
+		    u.compare();
+			System.out.println();
+		}
 
+	}
+}
 
 public class game {
 
 	public static void main(String[] args) {
-		umpire u = new umpire();
-		u.collectNumFromGuesser();
-		u.collectNumFromPlayer();
-		u.compare();
+		System.out.println(" ");
+		System.out.println("-------------------------Guesser Game-------------------------");
+		System.out.println();
+		int i=1;
+		String m;
+		Scanner sc =new Scanner(System.in);
+		while ( i==1) {
+			System.out.println("------do you want to play the Guesser game Y/N:-----------");
+			System.out.println();
+			m=sc.nextLine();
+			switch (m) {
+				case "y":
+				umpire um=new umpire();
+				um.countandmacth();
+			    break;
 
+			    case "n":
+				i++;
+				System.out.println("Game out");
+			    break;
+				default:
+				System.out.println("you choise is wrong:-");
+				break;
+			}
+		    
+		}
+		
+		
+		System.out.println();
+		System.out.println("-------------------------Guesser Game developed by Anshumaan khare-------------------------");
 	}
 
 }
